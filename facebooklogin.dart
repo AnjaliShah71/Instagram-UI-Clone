507e57b615061ac139f629066a105ce77c159f33
AnjaliShah_71 <anjalishah2823@gmail.com>
2026-09-14 23:30:45 +0530

    facebooklogin.dart

--- facebooklogin.dart
+++ facebooklogin.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
 
 class Facebooklogin extends StatefulWidget{
+  TextEditingController fuser = TextEditingController();
+  TextEditingController fpassword = TextEditingController();
   @override
   State<StatefulWidget> createState() =>_Facebooklogin();
 }
 
 class _Facebooklogin extends State<Facebooklogin>{
   @override
-  Widget build(BuildContext context) => Scaffold();
+  Widget build(BuildContext context) => Scaffold(
+  body: Center(
+      child: Column(
+        children: [
+          Icon(Icons.facebook_outlined, color: Colors.blueAccent,size: 44,),
+
+        ],
+      ),
+    ),
+  );
 }
